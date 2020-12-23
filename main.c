63a8016624815b64286cda7f751fdbead6b4ccc8
Alexander Voronov <avv.0@ya.ru>
2020-12-24 00:06:46 +0300

    Fix indentation style

--- main.c
+++ main.c
@@ -326,13 +326,13 @@ int force_dfu_gpio() {
 #define RCC_CFGR     (*(volatile uint32_t*)0x40021004U)
 
 #define RCC_CSR      (*(volatile uint32_t*)0x40021024U)
-#define RCC_CSR_LPWRRSTF			(1 << 31)
-#define RCC_CSR_WWDGRSTF			(1 << 30)
-#define RCC_CSR_IWDGRSTF			(1 << 29)
-#define RCC_CSR_SFTRSTF			(1 << 28)
-#define RCC_CSR_PORRSTF			(1 << 27)
-#define RCC_CSR_PINRSTF			(1 << 26)
-#define RCC_CSR_RMVF				(1 << 24)
+#define RCC_CSR_LPWRRSTF    (1 << 31)
+#define RCC_CSR_WWDGRSTF    (1 << 30)
+#define RCC_CSR_IWDGRSTF    (1 << 29)
+#define RCC_CSR_SFTRSTF     (1 << 28)
+#define RCC_CSR_PORRSTF     (1 << 27)
+#define RCC_CSR_PINRSTF     (1 << 26)
+#define RCC_CSR_RMVF        (1 << 24)
 
 #ifdef ENABLE_PINRST_DFU_BOOT
 static inline int reset_due_to_pin() {
