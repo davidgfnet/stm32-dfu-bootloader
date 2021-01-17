9cce4affb81d7067f254a70388317da4a4d476fe
Alexander Voronov <avv.0@ya.ru>
2021-01-17 13:19:42 +0300

    Add config header
    Config header allows customize some bootloader parameters like USB
    Device Descriptor fields in `user_config.h` file.

---
+++ user_config.h
@@ -0,0 +1,11 @@
+#ifndef USER_CONFIG_H
+#define USER_CONFIG_H
+
+//#define USB_VID 0x1209
+//#define USB_PID 0x4242
+
+//#define USB_MANUFACTURER "Yet Another Manufacturer"
+//#define USB_PRODUCT      "Yet Another Product"
+//#define USB_SERIALNUMBER "#000001"
+
+#endif //USER_CONFIG_H
